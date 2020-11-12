b042e39501268c10b870cb3c63cad5316c0a8fb4
hallegv <56059233+hallegv@users.noreply.github.com>
2020-11-11 18:52:12 -0800

    Create IntListInterface.java

---
+++ Classwork/IntLists/IntListInterface.java
@@ -0,0 +1,10 @@
+package intlist;
+
+public interface IntListInterface {
+
+    int  getAt (int index);
+    void append (int toAdd);
+    void insertAt (int toAdd, int index);
+    void removeAt (int index);
+    
+}
